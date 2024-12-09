84589a75fbe91607b762416c76f9adcbd7c02010
Billy <manhtran150394@gmail.com>
2024-12-09 09:30:10 +0700

    change setting

--- ssvscarper/ssvscarper/settings.py
+++ ssvscarper/ssvscarper/settings.py
@@ -12,31 +12,31 @@ BOT_NAME = "ssvscarper"
 SPIDER_MODULES = ["ssvscarper.spiders"]
 NEWSPIDER_MODULE = "ssvscarper.spiders"
 
-SCRAPEOPS_API_KEY = '3e993a5d-8b88-45c6-94c5-e776f6f435b2'
-SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT = 'https://headers.scrapeops.io/v1/user-agents'
-SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
-SCRAPEOPS_NUM_RESULTS = 400
+# SCRAPEOPS_API_KEY = '3e993a5d-8b88-45c6-94c5-e776f6f435b2'
+# SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT = 'https://headers.scrapeops.io/v1/user-agents'
+# SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
+# SCRAPEOPS_NUM_RESULTS = 400
 
-SCRAPEOPS_FAKE_BROWSER_HEADER_ENDPOINT = 'https://headers.scrapeops.io/v1/browser-headers'
-SCRAPEOPS_FAKE_BROWSER_HEADER_ENABLED = True
+# SCRAPEOPS_FAKE_BROWSER_HEADER_ENDPOINT = 'https://headers.scrapeops.io/v1/browser-headers'
+# SCRAPEOPS_FAKE_BROWSER_HEADER_ENABLED = True
 
-PROXY_POOL_ENABLED = True
+# PROXY_POOL_ENABLED = True
 
-ROTATING_PROXY_LIST = [
-    'http://sp96m864b3:aUpFZkew702omK_d5r@gate.smartproxy.com:10001',
-    'http://sp96m864b3:aUpFZkew702omK_d5r@gate.smartproxy.com:10002',
-]
+# ROTATING_PROXY_LIST = [
+#     'http://sp96m864b3:aUpFZkew702omK_d5r@gate.smartproxy.com:10001',
+#     'http://sp96m864b3:aUpFZkew702omK_d5r@gate.smartproxy.com:10002',
+# ]
 
-PROXY_USER = 'sp96m864b3'
-PROXY_PASSWORD = 'aUpFZkew702omK_d5r'
-PROXY_ENDPOINT = 'gate.smartproxy.com'
-PROXY_PORT = '10001'
+# PROXY_USER = 'sp96m864b3'
+# PROXY_PASSWORD = 'aUpFZkew702omK_d5r'
+# PROXY_ENDPOINT = 'gate.smartproxy.com'
+# PROXY_PORT = '10001'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "ssvscarper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 32
@@ -69,16 +69,17 @@ CONCURRENT_REQUESTS = 32
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-   "ssvscarper.middlewares.SsvscarperDownloaderMiddleware": 543,
-   # 'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-   # 'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
-   # 'ssvscarper.middlewares.SsvscraperFakeUserAgentMiddleware': 400,
-   'ssvscarper.middlewares.SsvscraperFakeBrowserHeaderAgentMiddleware': 400,
-   # 'ssvscarper.middlewares.SsvscraperProxyMiddleware': 350,
-   'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
-   'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
-}
+# DOWNLOADER_MIDDLEWARES = {
+#    "ssvscarper.middlewares.SsvscarperDownloaderMiddleware": 543,
+#    'ssvscarper.middlewares.SsvscraperFakeBrowserHeaderAgentMiddleware': 400,
+#    'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+#    'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+
+#    # 'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+#    # 'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+#    # 'ssvscarper.middlewares.SsvscraperFakeUserAgentMiddleware': 400,
+#    # 'ssvscarper.middlewares.SsvscraperProxyMiddleware': 350,
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -88,9 +89,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   "ssvscarper.pipelines.SsvscarperPipeline": 300,
-}
+# ITEM_PIPELINES = {
+#    "ssvscarper.pipelines.SsvscarperPipeline": 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
